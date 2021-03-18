c4a8c3760f9baad458ca2a94fdfd7aaa166e3119
Andrew Bernard Morgan <69614577+AMorgan4488@users.noreply.github.com>
2021-03-17 17:50:48 -0700

    Progress on view-recipe

--- RecipesFromTheHeart/src/app/app.module.ts
+++ RecipesFromTheHeart/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { ViewRecipeComponent } from './view-recipe/view-recipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    ViewRecipeComponent
   ],
   imports: [
     BrowserModule,
