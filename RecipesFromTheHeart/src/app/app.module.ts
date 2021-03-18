c36c94cd16c047b3da1b7890c2156f8a362ae58b
Lauren Kay <laurenkay7@gmail.com>
2021-03-17 17:28:19 -0700

    homepage progress

--- RecipesFromTheHeart/src/app/app.module.ts
+++ RecipesFromTheHeart/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    AboutComponent
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
