322aadc0d16829921840a5f0c673f6f82d54fb6b
Andrew Bernard Morgan <69614577+AMorgan4488@users.noreply.github.com>
2021-03-31 13:46:22 -0700

    this contains the breakfast, lunch, dinner components and routing

--- RecipesFromTheHeart/src/app/app-routing.module.ts
+++ RecipesFromTheHeart/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BreakfastComponent } from './breakfast/breakfast.component';
+import { DinnerComponent } from './dinner/dinner.component';
+import { FavoritesListComponent } from './favorites-list/favorites-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LunchComponent } from './lunch/lunch.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent},
+  { path: 'breakfast', component: BreakfastComponent},
+  { path: 'lunch', component: LunchComponent},
+  { path: 'dinner', component: DinnerComponent},
+  { path: 'favoriteslist', component: FavoritesListComponent},
+  { path: 'footer', component: FooterComponent},
+  { path: 'homepage', component: HomePageComponent },
+  { path: '', redirectTo: "/home", pathMatch:"full"},
+  { path: 'menubar', component: MenuBarComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  AboutComponent, FavoritesListComponent, FooterComponent, HomePageComponent,MenuBarComponent, BreakfastComponent, LunchComponent, DinnerComponent
+]
--- RecipesFromTheHeart/src/app/app.module.ts
+++ RecipesFromTheHeart/src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuBarComponent } from './menu-bar/menu-bar.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { ViewRecipeComponent } from './view-recipe/view-recipe.component';
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent,
-    HomePageComponent,
-    FooterComponent,
-    AboutComponent,
-    ViewRecipeComponent
+    routingComponents
+    
   ],
   imports: [
     BrowserModule,
